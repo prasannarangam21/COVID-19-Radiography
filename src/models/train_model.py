1800f2bc14e61037d29af53b11f2e879b05f1a85
Lakshmi Prasanna Rangam <LakshmiPrasannaRangam>
2020-09-21 23:09:40 -0600

    updated the file

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,24 +1,22 @@
-import joblib 
+import pandas as pd
+import joblib
 from keras.utils import np_utils
+from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import confusion_matrix, classification_report
 
-from tensorflow.keras.models import Model
-from tensorflow.keras.models import Sequential
+from tensorflow.keras import backend as k
+from tensorflow.keras.models import Model, Sequential
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications import VGG16
-from tensorflow.keras.layers import Input, Dense, Flatten, MaxPooling2D, GlobalAveragePooling2D, Dropout, BatchNormalization, Conv2D, InputLayer
-from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
+from tensorflow.keras.layers import Input, Dense, Flatten, Dropout
 
+from src.config import BATCH_SIZE, IMG_SIZE, LR, EPOCHS, LOG_DIR, CHECKPOINT_DIR, PROCESSED_DATA_PATH
 
-def target_preprocessing(y)
-    encoder = LabelEncoder()
-    encoded_labels = encoder.fit_transform(y)
-    y_encoded = np_utils.to_categorical(encoded_labels)
-    return y_encoded
 
 def VGG16_model():
     base = VGG16(include_top=False, weights='imagenet', input_shape=(IMG_SIZE, IMG_SIZE, 3))
@@ -33,44 +31,35 @@ def VGG16_model():
     
     return model
 
-y_encoded = target_preprocessing(y)
-X_train, X_cvtest, y_train, y_cvtest = train_test_split(X, y_encoded, test_size=0.40, stratify=y_encoded, random_state=42)
-X_cv, X_test, y_cv, y_test = train_test_split(X_cvtest, y_cvtest, test_size=0.50, stratify=y_cvtest, random_state=42)
+data = pd.read_csv(PROCESSED_DATA_PATH)
+data = shuffle(data)
 
-
-BATCH_SIZE = 64
-TRAIN_LENGTH = len(X_train)
-VAL_LENGTH = len(X_cv)
-STEPS_PER_EPOCH = TRAIN_LENGTH // BATCH_SIZE
-SPE_VAL = VAL_LENGTH // BATCH_SIZE
-IMG_SIZE = 224
-LR = 0.0001
-EPOCHS = 20
-
-datagen = ImageDataGenerator(rescale=1. / 255, validation_split=0.25,
-                             rotation_range=15,
+datagen = ImageDataGenerator(rescale=1. / 255, validation_split=0.2,
+                             rotation_range=25,
                              fill_mode="nearest")
 
-
 train_generator = datagen.flow_from_dataframe(
-    dataframe = data, directory = None,
+    dataframe = data,
     x_col = "path", y_col = "label",
-    target_size=(IMG_SIZE, IMG_SIZE),
-    batch_size=BATCH_SIZE,
-    shuffle=True,
-    subset='training',
-    class_mode="categorical"
+    target_size = (IMG_SIZE, IMG_SIZE),
+    batch_size = BATCH_SIZE,
+    shuffle = True,
+    subset = 'training',
+    class_mode = 'categorical'
     )
 
 validation_generator = datagen.flow_from_dataframe(
-    dataframe = data, directory = None,
+    dataframe = data,
     x_col = "path", y_col = "label",
-    target_size=(IMG_SIZE, IMG_SIZE),
-    batch_size=BATCH_SIZE,
-    shuffle=True,
+    target_size = (IMG_SIZE, IMG_SIZE),
+    batch_size = BATCH_SIZE,
+    shuffle = False,
     subset='validation',
-    class_mode="categorical"
+    class_mode='categorical'
     )
+
+TRAIN_SPE = train_generator.samples//BATCH_SIZE
+VAL_SPE = validation_generator.samples//BATCH_SIZE
 
 model = Sequential()
 model.add(VGG16_model())
@@ -82,30 +71,35 @@ model.add(Dense(3, activation='sigmoid'))
 
 # model.summary()
 
-tf.keras.backend.clear_session()
+k.clear_session()
 
 print("Training Model")
 optimizer = Adam(lr = LR, decay = LR/EPOCHS)
 
 model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 
-earlystop = EarlyStopping(monitor='val_accuracy', min_delta = 0.002, patience = 5 ,mode = 'auto', verbose = 1)
+earlystop = EarlyStopping(monitor='val_accuracy', min_delta = 0.002, 
+                          patience = 15, mode = 'auto', verbose = 1)
 
-checkpointer = ModelCheckpoint(filepath = 'output/models/snapshots/model-{epoch:03d}-{val_accuracy:03f}.h5', 
-                               monitor = 'val_accuracy', verbose = 1, save_best_only = True, mode ='auto')
+tensorboard_callback =TensorBoard(log_dir=LOG_DIR, histogram_freq=1)
 
-callbacks_list = [earlystop, checkpointer]
+checkpointer = ModelCheckpoint(filepath = CHECKPOINT_DIR, monitor = 'val_accuracy', 
+                               verbose = 0, save_best_only = True, mode ='auto')
 
-history = model.fit(train_generator, batch_size=BATCH_SIZE,
-                                    steps_per_epoch=STEPS_PER_EPOCH,
-                                    validation_data=validation_generator,
-                                    epochs=EPOCHS,
-                                    verbose=1, callbacks = callbacks_list)
+callbacks_list = [earlystop, checkpointer, tensorboard_callback]
+
+history = model.fit(train_generator, 
+                    batch_size=BATCH_SIZE,
+                    steps_per_epoch=TRAIN_SPE,
+                    validation_data=validation_generator,
+                    validation_steps=VAL_SPE,
+                    epochs=EPOCHS,
+                    verbose=1, callbacks = callbacks_list)
 
 print("Training Completed")
-print("Saving Model as model.h5 in /output/models directory")
-model.save('/content/output/models/model.h5')
+print("Saving Model as model.h5 in output/models directory")
+model.save('output/models/model.h5')
   
 # Save the model as a pickle in a file
 print("Saving model metrics as output/history.pkl")
-joblib.dump(history.history, 'output/history.pkl') 
+joblib.dump(history.history, 'output/history.pkl')
